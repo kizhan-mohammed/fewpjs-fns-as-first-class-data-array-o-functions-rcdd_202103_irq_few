e643a5a8b11ab566d97997df4ccb60b97a5463f7
Kizhan Mohammed <kizhan.m.kareem@gmail.com>
2021-04-05 10:17:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function walkHome(dogName, dogBreed) {
 function unleashDog(dogName, dogBreed) {
   console.log(`Unleash ${dogName} the ${dogBreed}`);
 }
-
+wakeDog("Xerox", ")
 let routine = ["wakeDog", "leashDog", "walkToPark", "throwFrisbee", "walkHome", "unleashDog"]
 function exerciseDog(routine){
   console.log(routine);
