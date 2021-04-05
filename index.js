ca82a7af48bf13f7b0d03022c4b3a95eba89c712
Kizhan Mohammed <kizhan.m.kareem@gmail.com>
2021-04-05 10:00:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 function wakeDog(dogName, dogBreed) {
   console.log(`Wake ${dogName} the ${dogBreed}`);
 }
+function wlahsDog(dogName, dogBreed) {
+  console.log(`Wake ${dogName} the ${dogBreed}`);
+}
+function wakeDog(dogName, dogBreed) {
+  console.log(`Wake ${dogName} the ${dogBreed}`);
+}
+function wakeDog(dogName, dogBreed) {
+  console.log(`Wake ${dogName} the ${dogBreed}`);
+}
